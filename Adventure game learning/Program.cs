a3cff57f8a2ffc3c45b5f30ef23b64529fca7244
Richard Layman <richard.t.layman@gmail.com>
2019-03-12 16:16:09 -0400

    starting out

--- Adventure game learning/Program.cs
+++ Adventure game learning/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿// Learning from http://programmingisfun.com/learn/c-sharp-adventure-game/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Adventure_game_learning
 {
@@ -10,6 +13,16 @@ namespace Adventure_game_learning
     {
         static void Main(string[] args)
         {
+            string CharacterName = "John Doe";
+
+            Console.WriteLine("My Adventure Game.");
+            Console.ReadKey();
+
+            Console.WriteLine("Please name your character:");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Welcome to the game " + CharacterName + ".");
+
+            
         }
     }
 }
